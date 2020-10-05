b192a57f6ba01285aeace93c3aca728e9f8caa07
kavinsurya <kavinsurya002@gmail.com>
2020-10-05 10:09:28 +0530

    api call

--- js/script.js
+++ js/script.js
@@ -1,148 +1,169 @@
-var navbar = document.createElement("nav");
-navbar.className = "navbar navbar-expand-lg navbar-dark bg-dark";
+function navbar(){
+    var topics = ['home', 'world','politics','magazine', 'technology','science','health','sports','arts','fashion','food','travel'];
+    var header = document.createElement('header');
+    var nav = document.createElement('nav');
+    nav.setAttribute('class', 'navbar navbar-expand-xl navbar-dark bg-dark');
+   
+    var navHeader = document.createElement("a");
+     navHeader.className= "navbar-brand";
+     navHeader.setAttribute("onclick", "getNews('home')");
+     navHeader.innerText = "New York Times";
+   
+     var button = document.createElement("button");
+     button.className=  "navbar-toggler";
+     button.type= "button";
+     button.setAttribute("data-toggle", "collapse");
+     button.setAttribute("data-target", "#navbarCollapse");
+     button.setAttribute("aria-controls", "navbarCollapse");
+     button.setAttribute("aria-expanded", "false");
+     button.setAttribute("aria-label", "Toggle navigation");
+   
+     var span = document.createElement("span");
+     span.setAttribute("class", "navbar-toggler-icon");
+     button.appendChild(span);
+   
+     var navbarDiv = document.createElement("div");
+     navbarDiv.setAttribute("class", "collapse navbar-collapse");
+     navbarDiv.setAttribute("id", "navbarCollapse");
+   
+     var ul = document.createElement("ul");
+     ul.setAttribute("class", "navbar-nav ");
+   
+   for(let i = 0; i< topics.length; i++){
+     
+     var li = document.createElement("li");
+     li.setAttribute("class", "nav-item text-uppercase");
+     li.id = topics[i];
+   
+     var a = document.createElement("a");
+     a.setAttribute("class", "nav-link");
+     a.setAttribute("onclick", "getNews('"+topics[i]+"')");
+     a.innerText = topics[i];
+     li.appendChild(a);
+     ul.appendChild(li);
+   }
+     
+     navbarDiv.appendChild(ul);
+     nav.append(navHeader, button, navbarDiv);
+     header.appendChild(nav);
+     return header;
+   }
+   
 
-var headerName = document.createElement("a");
-headerName.className = "navbar-brand";
-headerName.innerText = "New York Times";
+function container() {
+  var container = document.createElement("div");
+  container.className = "container";
+  container.id = "data";
+  return container;
+}
 
-var button = document.createElement("button");
-button.className = "navbar-toggler";
-button.type = "button";
-button.setAttribute("data-toggle", "collapse");
-button.setAttribute("data-target", "#navbarNav");
-button.setAttribute("aria-controls", "navbarNav");
-button.setAttribute("aria-expanded", "false");
-button.setAttribute("aria-label", "Toggle navigation");
+async function getNews(type) {
+  try {
+    var data = await fetch(
+      "https://api.nytimes.com/svc/topstories/v2/" +
+        type +
+        ".json?api-key=qLTWAwQAbc8JsIm5XX5GlHJhlkGAOqOl"
+    );
+    if (data.status == 200 && data.statusText == "OK") {
+      var json = await data.json();
+      createCard(type, json);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
 
-var buttonSpan = document.createElement("span");
-buttonSpan.className = "navbar-toggler-icon";
-button.append(buttonSpan);
+async function createCard(type, json) {
+  document.getElementById("data").innerHTML = "";
+  var container = document.getElementById("data");
+  document.getElementById(type);
 
-var navbarDiv = document.createElement("div");
-navbarDiv.className = "collapse navbar-collapse";
-navbarDiv.id = "navbarNav";
+  var rowDiv = document.createElement("div");
+  rowDiv.className = "row";
 
-var ul = document.createElement("ul");
-ul.className = "navbar-nav";
+  for (let i = 0; i < json.results.length; i++) {
+    var col = document.createElement("div");
+    col.className = "col-12 colStyle";
+    var card = document.createElement("div");
+    card.className = "card";
+    var row = document.createElement("div");
+    row.className = "row ";
 
-var li1 = document.createElement("li");
-li1.className = "nav-item";
+    var content = document.createElement("div");
+    content.className = "col-lg-8 col-sm-12";
 
-var a1 = document.createElement("a");
-a1.className = "nav-link";
+    var bodyContent = document.createElement("div");
+    bodyContent.className = "card-body";
+    var section = document.createElement("h4");
+    section.setAttribute("class", "card-title text-uppercase section-card");
+    section.innerHTML = json.results[i].section;
 
-a1.id = "";
-a1.innerText = "HOME";
-li1.setAttribute("onclick", 'getStarted("science")');
-li1.append(a1);
+    var title = document.createElement("h5");
+    title.className = "card-title titlecard";
+    title.innerHTML = json.results[i].title;
 
-var li2 = document.createElement("li");
-li2.className = "nav-item";
+    var createdDate = document.createElement("p");
+    createdDate.className = "card-text  date-card";
 
-var a2 = document.createElement("a");
-a2.className = "nav-link";
-a2.id = "";
-a2.innerText = "WORLD";
+    var date = document.createElement("small");
+    date.className = "text-muted";
+    date.innerText = formatDate(json.results[i].created_date);
 
-li2.append(a2);
+    createdDate.appendChild(date);
 
-var li3 = document.createElement("li");
-li3.className = "nav-item";
+    var abstract = document.createElement("p");
+    abstract.className = "card-text abstract-card";
+    abstract.innerText = json.results[i].abstract;
 
-var a3 = document.createElement("a");
-a3.className = "nav-link";
-a3.id = "";
-a3.innerText = "POLITICS";
-li3.append(a3);
+    var redirectLink = document.createElement("a");
+    redirectLink.className = "card-link continueReading";
+    redirectLink.href = json.results[i].short_url;
+    redirectLink.innerText = "Continue Reading";
+    redirectLink.target = "_blank";
 
-var li4 = document.createElement("li");
-li4.className = "nav-item";
+    bodyContent.append(section, title, createdDate, abstract, redirectLink);
+    content.appendChild(bodyContent);
 
-var a4 = document.createElement("a");
-a4.className = "nav-link";
-a4.id = "";
-a4.innerText = "MAGAZINE";
+    var imageDiv = document.createElement("div");
+    imageDiv.className = "col-lg-4 col-sm-12";
 
-li4.append(a4);
+    var img = document.createElement("img");
+    img.className = "card-img   imgStyle";
+    img.src = json.results[i].multimedia[0].url;
+    imageDiv.appendChild(img);
 
-var li5 = document.createElement("li");
-li5.className = "nav-item";
+    
+    row.append(content, imageDiv);
+    card.append(row);
+    col.append(card);
+    rowDiv.append(col);
+  }
+  container.append(rowDiv);
+}
 
-var a5 = document.createElement("a");
-a5.className = "nav-link";
-a5.id = "";
-a5.innerText = "TECHNOLOGY";
-li5.append(a5);
+function formatDate(dateString) {
+  var months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let newDate = new Date(dateString);
+  let date = months[newDate.getMonth()];
+  +"," + newDate.getFullYear();
+  console.log(date);
+  return date;
+}
 
-var li6 = document.createElement("li");
-li6.className = "nav-item";
-
-var a6 = document.createElement("a");
-a6.className = "nav-link";
-a6.id = "";
-a6.innerText = "SCIENCE";
-
-li6.append(a6);
-
-var li7 = document.createElement("li");
-li7.className = "nav-item";
-
-var a7 = document.createElement("a");
-a7.className = "nav-link";
-a7.id = "";
-a7.innerText = "HEALTH";
-li7.append(a7);
-
-var li8 = document.createElement("li");
-li8.className = "nav-item";
-
-var a8 = document.createElement("a");
-a8.className = "nav-link";
-a8.id = "";
-a8.innerText = "SPORTS";
-
-li8.append(a8);
-
-var li9 = document.createElement("li");
-li9.className = "nav-item";
-
-var a9 = document.createElement("a");
-a9.className = "nav-link";
-a9.id = "";
-a9.innerText = "ARTS";
-
-li9.append(a9);
-
-var li10 = document.createElement("li");
-li10.className = "nav-item";
-
-var a10 = document.createElement("a");
-a10.className = "nav-link";
-a10.id = "";
-a10.innerText = "FASHION";
-li10.append(a10);
-
-var li11 = document.createElement("li");
-li11.className = "nav-item";
-
-var a11 = document.createElement("a");
-a11.className = "nav-link";
-a11.id = "";
-a11.innerText = "FOOD";
-
-li11.append(a11);
-
-var li12 = document.createElement("li");
-li12.className = "nav-item";
-
-var a12 = document.createElement("a");
-a12.className = "nav-link";
-a12.id = "";
-a12.innerText = "TRAVEL";
-
-li12.append(a12);
-
-ul.append(li1, li2, li3, li4, li5, li6, li7, li8, li9, li10, li11, li12);
-navbarDiv.append(ul);
-navbar.append(headerName, button, navbarDiv);
-document.body.append(navbar);
+document.body.append(navbar(),container());
+window.onload = (event) => {
+  getNews("world");
+};
